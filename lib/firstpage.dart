19013a2d2d3371eb5dc28da41a3e85a8c3006094
Malsaeed276 <mohamed.elsaeed276@gmail.com>
2020-05-21 01:42:42 +0300

    first page style part 1

---
+++ lib/firstpage.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+
+class firstpage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+
+    return MyspellChecker();
+  }
+}
+class MyspellChecker extends State<firstpage>
+{
+
+  final TextEditingController _controller = new TextEditingController();
+  //final myController = TextEditingController();
+  String result = "";
+  double resultSize;
+
+  void haveTheAnswer(String input) {
+    setState(() {
+      if (input == " " || input == null || input == "") {
+        result = " you did not enter any word";
+      } else {
+        result = input;
+      }
+
+      resultSize = result.length.toDouble();
+    });
+    _controller.text = "";
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Colors.orange,
+                ),
+                padding: EdgeInsets.all(8.0),
+                width: 600.0,
+                margin: const EdgeInsets.fromLTRB(20, 2, 20, 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: ('enter your word'),
+                  ),
+                  onSubmitted: haveTheAnswer,
+                  controller: _controller,
+                ),
+              ),
+
+              // result of the word
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(5.5)),
+
+                //it should be with the length of the word
+                margin: const EdgeInsets.fromLTRB(50, 2, 50, 2),
+                padding: EdgeInsets.all(5),
+                child: Center(
+                  child: Text(result),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spellchecker/firstpage.dart';
 import 'Dictionary.dart';
 
 void main() => runApp(MyApp());
@@ -6,76 +7,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-
     return MyAppState();
   }
 }
 
 class MyAppState extends State<MyApp> {
-  final TextEditingController _controller = new TextEditingController();
-  //final myController = TextEditingController();
-  String result = "";
-  double resultSize;
-
-  void haveTheAnswer(String input) {
-    setState(() {
-      if (input == " " || input == null || input == "") {
-        result = " you did not enter any word";
-      } else {
-
-        result = input;
-      }
-
-      resultSize = result.length.toDouble();
-    });
-    _controller.text = "";
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Spell checker'),
-      ),
-      body: Container(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                color: Colors.orange,
-              ),
-              padding: EdgeInsets.all(8.0),
-              width: 600.0,
-              margin: const EdgeInsets.fromLTRB(20, 2, 20, 10),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: ('enter your word'),
-                ),
-                onSubmitted: haveTheAnswer,
-                controller: _controller,
-              ),
-            ),
-
-            // result of the word
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(5.5)),
-
-              //it should be with the length of the word
-              margin: const EdgeInsets.fromLTRB(50, 2, 50, 2),
-              padding: EdgeInsets.all(5),
-              child: Center(
-                child: Text(result),
-              ),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Spell checker'),
         ),
-      )),
-    ));
+        body: firstpage(),
+      ),
+    );
   }
 }
